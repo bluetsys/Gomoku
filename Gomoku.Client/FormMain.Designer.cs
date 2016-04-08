07ffd74e47e6fb63ff2fb68ba10e45faff99239f
WooYoung Moon <wymoon2690@gmail.com>
2016-04-08 19:02:41 +0900

    초기 코드

---
+++ Gomoku.Client/FormMain.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Gomoku.Client
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxBoard = new System.Windows.Forms.PictureBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonReStart = new System.Windows.Forms.Button();
+            this.buttonUnDo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxBoard
+            // 
+            this.pictureBoxBoard.Location = new System.Drawing.Point(13, 13);
+            this.pictureBoxBoard.Name = "pictureBoxBoard";
+            this.pictureBoxBoard.Size = new System.Drawing.Size(570, 570);
+            this.pictureBoxBoard.TabIndex = 0;
+            this.pictureBoxBoard.TabStop = false;
+            this.pictureBoxBoard.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxBoard_MouseMove);
+            this.pictureBoxBoard.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxBoard_MouseUp);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(693, 13);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(612, 13);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonReStart
+            // 
+            this.buttonReStart.Location = new System.Drawing.Point(693, 42);
+            this.buttonReStart.Name = "buttonReStart";
+            this.buttonReStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonReStart.TabIndex = 3;
+            this.buttonReStart.Text = "ReStart";
+            this.buttonReStart.UseVisualStyleBackColor = true;
+            this.buttonReStart.Click += new System.EventHandler(this.buttonReStart_Click);
+            // 
+            // buttonUnDo
+            // 
+            this.buttonUnDo.Location = new System.Drawing.Point(693, 71);
+            this.buttonUnDo.Name = "buttonUnDo";
+            this.buttonUnDo.Size = new System.Drawing.Size(75, 23);
+            this.buttonUnDo.TabIndex = 4;
+            this.buttonUnDo.Text = "UnDo";
+            this.buttonUnDo.UseVisualStyleBackColor = true;
+            this.buttonUnDo.Click += new System.EventHandler(this.buttonUnDo_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 599);
+            this.Controls.Add(this.buttonUnDo);
+            this.Controls.Add(this.buttonReStart);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.pictureBoxBoard);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMain_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBoard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxBoard;
+		private System.Windows.Forms.Button buttonSave;
+		private System.Windows.Forms.Button buttonLoad;
+		private System.Windows.Forms.Button buttonReStart;
+        private System.Windows.Forms.Button buttonUnDo;
+    }
+}
+
---
+++ Gomoku.Client/FormMain.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Gomoku.Client
+{
+    public partial class FormMain : Form
+    {
+        private Session m_Session = new Session();
+        private int m_Margin = 30;
+        private Color lineColor = Color.FromArgb(0, 0, 0);
+        private int m_LineCount = 19;
+
+        public FormMain()
+        {
+            this.InitializeComponent();
+            this.m_Margin = this.pictureBoxBoard.Width / this.m_LineCount;
+            this.RePaintGoBoard();
+            this.m_Session.Start();
+        }
+
+        private void NewRePaintGoBoard()
+        {
+            this.pictureBoxBoard.Refresh();
+        }
+
+        private void RePaintGoBoard()
+        {
+            Bitmap _Bitmap = new Bitmap(this.pictureBoxBoard.Width, this.pictureBoxBoard.Height);
+            using (Graphics _Graphics = Graphics.FromImage(_Bitmap))
+            {
+                Pen _Pen = new Pen(lineColor, 3);
+                Console.WriteLine("{0}, {1}, {2}, {3}", this.m_Margin / 2, this.m_Margin / 2, this.m_Margin / 2, this.m_Margin * this.m_LineCount - this.m_Margin / 2);
+                _Graphics.DrawLine(_Pen, this.m_Margin / 2, this.m_Margin / 2, this.m_Margin / 2, this.m_Margin * this.m_LineCount - this.m_Margin / 2);
+                _Graphics.DrawLine(_Pen, this.m_Margin / 2, this.m_Margin / 2, this.m_Margin * this.m_LineCount - this.m_Margin / 2, this.m_Margin / 2);
+                _Graphics.DrawLine(_Pen, this.m_Margin / 2, this.m_Margin * this.m_LineCount - this.m_Margin / 2, this.m_Margin * this.m_LineCount - this.m_Margin / 2, this.m_Margin * this.m_LineCount - this.m_Margin / 2);
+                _Graphics.DrawLine(_Pen, this.m_Margin * this.m_LineCount - this.m_Margin / 2, this.m_Margin / 2, this.m_Margin * this.m_LineCount - this.m_Margin / 2, this.m_Margin * this.m_LineCount - this.m_Margin / 2);
+
+                _Pen = new Pen(lineColor, 1);
+                for (int i = this.m_Margin + this.m_Margin / 2; i < this.m_Margin * m_LineCount - this.m_Margin / 2; i += this.m_Margin)
+                {
+                    _Graphics.DrawLine(_Pen, this.m_Margin / 2, i, this.m_Margin * m_LineCount - this.m_Margin / 2, i);
+                    _Graphics.DrawLine(_Pen, i, this.m_Margin / 2, i, this.m_Margin * m_LineCount - this.m_Margin / 2);
+                }
+
+                int _n = 10;//점크기
+                            // 9군데 점
+                int _p1 = 4 * this.m_Margin - 20;
+                int _p2 = (this.m_LineCount + 1) * this.m_Margin / 2 - 20;
+                int _p3 = (this.m_LineCount - 3) * this.m_Margin - 20;
+                // this.pictureBoxBoard.BackColor = Color.Yellow;
+
+                // 9군데 점찍기
+                _Graphics.FillEllipse(Brushes.Black, _p1, _p1, _n, _n);
+                _Graphics.FillEllipse(Brushes.Black, _p2, _p1, _n, _n);
+                _Graphics.FillEllipse(Brushes.Black, _p3, _p1, _n, _n);
+                _Graphics.FillEllipse(Brushes.Black, _p1, _p2, _n, _n);
+                _Graphics.FillEllipse(Brushes.Black, _p2, _p2, _n, _n);
+                _Graphics.FillEllipse(Brushes.Black, _p3, _p2, _n, _n);
+                _Graphics.FillEllipse(Brushes.Black, _p1, _p3, _n, _n);
+                _Graphics.FillEllipse(Brushes.Black, _p2, _p3, _n, _n);
+                _Graphics.FillEllipse(Brushes.Black, _p3, _p3, _n, _n);
+
+                this.pictureBoxBoard.Image = _Bitmap;
+            }
+        }
+
+        private void pictureBoxBoard_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.Cursor = Cursors.Default;
+
+            int _X = (e.Location.X / this.m_Margin) + 1;
+            int _Y = (e.Location.Y / this.m_Margin) + 1;
+
+            if (this.m_Session.List.Where(r => r.X == _X && r.Y == _Y).Count() == 0)
+            {
+                base.Cursor = Cursors.Hand;
+            }
+        }
+
+        private void NewMethod(Graphics graphics, Model model, bool undo = false, int size = 26)
+        {
+            int _XPoint = (this.m_Margin) * model.X - this.m_Margin + (this.m_Margin - size) / 2;
+            int _YPoint = (this.m_Margin) * model.Y - this.m_Margin + (this.m_Margin - size) / 2;
+
+            Bitmap _Bitmap = new Bitmap(size, size);
+            Graphics _Graphics = Graphics.FromImage(_Bitmap);
+
+            if (!undo)
+            {
+                var _Turn1 = new System.Drawing.SolidBrush(Color.FromArgb(0, 0, 0));
+                var _Turn2 = new System.Drawing.SolidBrush(Color.FromArgb(255, 255, 255));
+
+                if (model.IsBlack)
+                {
+                    _Turn1 = new System.Drawing.SolidBrush(Color.FromArgb(255, 255, 255));
+                    _Turn2 = new System.Drawing.SolidBrush(Color.FromArgb(0, 0, 0));
+                }
+
+                _Graphics.FillEllipse(_Turn1, 0, 0, size, size);
+                _Graphics.DrawString(model.Index.ToString("000"), new Font("Arial", 10), _Turn2, 1, 6);
+            }
+            else
+            {
+                var _Pen = new Pen(lineColor, 1);
+                _Graphics.FillEllipse(new System.Drawing.SolidBrush(this.pictureBoxBoard.BackColor), 0, 0, size, size);
+                _Graphics.DrawLine(_Pen, size / 2, 0, size / 2, size);
+                _Graphics.DrawLine(_Pen, 0, size / 2, size, size / 2);
+            }
+
+            graphics.DrawImage(_Bitmap, _XPoint, _YPoint);
+        }
+
+        private void FormMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.Cursor = Cursors.Default;
+        }
+
+        private void pictureBoxBoard_MouseUp(object sender, MouseEventArgs e)
+        {
+            int _X = (e.Location.X / this.m_Margin) + 1;
+            int _Y = (e.Location.Y / this.m_Margin) + 1;
+
+            if (this.m_Session.List.Where(r => r.X == _X && r.Y == _Y).Count() != 0)
+            {
+                return;
+            }
+
+            Model _Model = new Model();
+
+            _Model.X = _X;
+            _Model.Y = _Y;
+
+            _Model.IsBlack = this.m_Session.Turn;
+
+            this.m_Session.PlayTurn(_Model);
+
+            this.NewMethod(pictureBoxBoard.CreateGraphics(), _Model);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            var _FileDialog = new SaveFileDialog();
+
+            _FileDialog.InitialDirectory = "c:\\";
+            _FileDialog.Filter = "gomoku play file (*.gdp)|*.gdp|All files (*.*)|*.*";
+            _FileDialog.FilterIndex = 1;
+            _FileDialog.RestoreDirectory = true;
+
+            if (_FileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            this.m_Session.Save(_FileDialog.FileName);
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            var _FileDialog = new OpenFileDialog();
+
+            _FileDialog.InitialDirectory = "c:\\";
+            _FileDialog.Filter = "gomoku play file (*.gdp)|*.gdp|All files (*.*)|*.*";
+            _FileDialog.FilterIndex = 1;
+            _FileDialog.RestoreDirectory = true;
+
+            if (_FileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            this.m_Session.Load(_FileDialog.FileName);
+
+            this.NewRePaintGoBoard();
+            foreach (var item in this.m_Session.List)
+            {
+                this.NewMethod(this.pictureBoxBoard.CreateGraphics(), item);
+            }
+        }
+
+        private void buttonReStart_Click(object sender, EventArgs e)
+        {
+            this.RePaintGoBoard();
+            this.m_Session.Start();
+        }
+
+        private void buttonUnDo_Click(object sender, EventArgs e)
+        {
+            // this.NewRePaintGoBoard();
+            this.NewMethod(this.pictureBoxBoard.CreateGraphics(), this.m_Session.List.LastOrDefault(), true);
+            this.m_Session.PlayUnDo();
+            //foreach (var item in this.m_Session.List)
+            //{
+            //    this.NewMethod(this.pictureBoxBoard.CreateGraphics(), item);
+            //}
+        }
+    }
+
+    [Serializable()]
+    public class Session
+    {
+        private System.Diagnostics.Stopwatch m_Stopwatch = new System.Diagnostics.Stopwatch();
+        private List<Model> m_List = null;
+
+        public Session()
+        {
+            this.m_List = new List<Model>();
+        }
+
+        public void Start()
+        {
+            this.m_List.Clear();
+            this.m_Stopwatch.Start();
+        }
+
+        public void Save(string filename)
+        {
+            XmlSerializer _XmlSerializer = new XmlSerializer(typeof(List<Model>));
+            TextWriter _TextWriter = new StreamWriter(filename);
+            _XmlSerializer.Serialize(_TextWriter, this.m_List);
+            _TextWriter.Close();
+        }
+
+        public void Load(string filename)
+        {
+            FileStream _FileStream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            XmlSerializer _XmlSerializer = new XmlSerializer(typeof(List<Model>));
+            List<Model> _Model = (List<Model>)_XmlSerializer.Deserialize(_FileStream);
+
+            this.m_List = _Model;
+
+            _FileStream.Close();
+        }
+
+        public void PlayUnDo()
+        {
+            if (this.m_List.Count == 0)
+            {
+                return;
+            }
+
+            this.Turn = !this.Turn;
+            this.m_List.Remove(this.m_List.Last());
+        }
+
+        public void PlayTurn(Model model)
+        {
+            this.m_Stopwatch.Stop();
+
+            this.Turn = !this.Turn;
+            model.Elapsed = this.m_Stopwatch.Elapsed.ToString();
+            model.Index = this.Index;
+            this.m_List.Add(model);
+            this.m_Stopwatch.Restart();
+        }
+
+        public int Index
+        {
+            get
+            {
+                return this.List.Count() + 1;
+            }
+
+        }
+
+        public bool Turn { get; set; }
+
+        public Model[] List
+        {
+            get
+            {
+                return this.m_List.ToArray();
+            }
+        }
+    }
+
+    public class Model
+    {
+        [XmlAttribute]
+        public string Elapsed { get; set; }
+
+        [XmlAttribute]
+        public int Index { get; set; }
+
+        [XmlAttribute]
+        public int X { get; set; }
+
+        [XmlAttribute]
+        public int Y { get; set; }
+
+        [XmlAttribute]
+        public bool IsBlack { get; set; }
+    }
+}
